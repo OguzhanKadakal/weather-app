0734a4399edac828ff339e41b2b2c19242381fa3
Oğuzhan Kadakal <76901135+OguzhanKadakal@users.noreply.github.com>
2025-06-19 22:59:31 +0300

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+// config/webpack.common.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      title: 'Webpack Template',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
+};
